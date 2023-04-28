62b77151ba69660084ee877e103dc19c21293380
cyeow <cheryl.ylt@gmail.com>
2023-04-28 08:43:10 +0800

    cleaned up import and spacing

--- src/main/java/ibf2022/paf/assessment/server/repositories/DBQueries.java
+++ src/main/java/ibf2022/paf/assessment/server/repositories/DBQueries.java
@@ -5,6 +5,5 @@ public class DBQueries {
     public static final String SELECT_USER_BY_USERNAME = "SELECT * FROM user WHERE username = ?;";
     
     public static final String INSERT_USER = "INSERT INTO user (user_id, username, name) VALUES (?, ?, ?);";
-
     public static final String INSERT_TASK = "INSERT INTO task (description, priority, due_date, user_id) VALUES (?, ?, ?, ?);";
 }
